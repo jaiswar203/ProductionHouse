da43487a328fc8ad9870d78467f21c61cd583ff2
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-01-05 12:50:40 +0530

    updated

---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Head from 'next/head'
+// import { Navbar, Footer } from '.'
+
+const Layout = ({ title, children, description }) => {
+    
+    return (
+        <div>
+            <Head>
+                <title>{title ? `${title} - Standalone` : 'StandAlone'}</title>
+                {description && <meta name='description' content={description} />}
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            </Head>
+            {/* <Navbar /> */}
+            <main >
+                {children}
+            </main>
+            {/* <Footer /> */}
+        </div>
+    )
+}
+
+export default Layout
